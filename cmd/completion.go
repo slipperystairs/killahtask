89e4248153c5f1b7d5f49ef1a71cde4fcac1548a
dcalligy <dcalligy813@gmail.com>
2026-01-22 20:22:02 -0500

    Adding tab completion

---
+++ cmd/completion.go
@@ -0,0 +1,30 @@
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var completionCmd = &cobra.Command{
+    Use:   "completion [bash|zsh|fish|powershell]",
+    Short: "Generate completion script",
+    Args:  cobra.ExactValidArgs(1),
+    ValidArgs: []string{"bash", "zsh", "fish", "powershell"},
+    Run: func(cmd *cobra.Command, args []string) {
+        switch args[0] {
+        case "bash":
+            rootCmd.GenBashCompletion(os.Stdout)
+        case "zsh":
+            rootCmd.GenZshCompletion(os.Stdout)
+        case "fish":
+            rootCmd.GenFishCompletion(os.Stdout, true)
+        case "powershell":
+            rootCmd.GenPowerShellCompletion(os.Stdout)
+        }
+    },
+}
+
+func init() {
+    rootCmd.AddCommand(completionCmd)
+}
